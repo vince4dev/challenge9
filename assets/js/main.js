653971fdf172a2c250ae195ac3cb2fa4b1fcdadb
vince4dev <vincent.bollier@icloud.com>
2026-05-15 17:48:46 +0200

    Updated CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector(".card__btn");
 
-btn.addEventListener("click", () => {
-  const isOpen = btn.classList.toggle("active");
-  btn.setAttribute("aria-expanded", isOpen);
-});
+if (btn) {
+  btn.addEventListener("click", () => {
+    const isOpen = btn.classList.toggle("active");
+    btn.setAttribute("aria-expanded", isOpen);
+  });
+}
